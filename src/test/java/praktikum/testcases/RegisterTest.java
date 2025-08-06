3c086447fad27d236d2d49a05c387d7dfa067743
rediyss <reddreynoklik@yandex.ru>
2025-08-06 12:57:16 +1100

    тесты на регистрацию

---
+++ src/test/java/praktikum/testcases/RegisterTest.java
@@ -0,0 +1,25 @@
+
+package praktikum.testcases;
+
+import org.junit.Test;
+import praktikum.base.BaseTest;
+import praktikum.page.RegisterPage;
+
+import static org.junit.Assert.assertTrue;
+
+public class RegisterTest extends BaseTest {
+
+    @Test
+    public void successRegistration() {
+        driver.get("https://stellarburgers.nomoreparties.site/register");
+        String email = "test" + System.currentTimeMillis() + "@example.com";
+        new RegisterPage(driver).register("Test User", email, "123456");
+    }
+
+    @Test
+    public void errorWhenShortPassword() {
+        driver.get("https://stellarburgers.nomoreparties.site/register");
+        new RegisterPage(driver).register("Test User", "short@example.com", "123");
+        assertTrue(new RegisterPage(driver).getErrorMessage().contains("Некорректный пароль"));
+    }
+}
