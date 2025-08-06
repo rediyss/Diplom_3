2bf7a611ebaa3de1df908f0bf77f98b55c95e025
rediyss <reddreynoklik@yandex.ru>
2025-08-06 12:56:36 +1100

    тесты на выбор табов

---
+++ src/test/java/praktikum/testcases/ConstructorTabTest.java
@@ -0,0 +1,31 @@
+package praktikum.testcases;
+
+import org.junit.Test;
+import praktikum.base.BaseTest;
+import praktikum.page.MainPage;
+
+import static org.junit.Assert.assertTrue;
+
+public class ConstructorTabTest extends BaseTest {
+
+    @Test
+    public void userCanSwitchToBunsTab() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickBunTab();
+        assertTrue("Булки не активны", mainPage.isBunTabActive());
+    }
+
+    @Test
+    public void userCanSwitchToSaucesTab() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickSauceTab();
+        assertTrue("Соусы не активны", mainPage.isSauceTabActive());
+    }
+
+    @Test
+    public void userCanSwitchToFillingsTab() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickFillingTab();
+        assertTrue("Начинки не активны", mainPage.isFillingTabActive());
+    }
+}
