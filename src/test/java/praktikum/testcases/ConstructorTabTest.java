ec6b2ccaa437236910bb5f3d4e02604dea3e3c9d
rediyss <reddreynoklik@yandex.ru>
2025-08-07 18:37:56 +1100

    Description и DisplayName добавлены

--- src/test/java/praktikum/testcases/ConstructorTabTest.java
+++ src/test/java/praktikum/testcases/ConstructorTabTest.java
@@ -1,5 +1,7 @@
 package praktikum.testcases;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
 import praktikum.base.BaseTest;
 import praktikum.page.MainPage;
@@ -9,6 +11,8 @@ import static org.junit.Assert.assertTrue;
 public class ConstructorTabTest extends BaseTest {
 
     @Test
+    @DisplayName("Переключение на вкладку 'Булки'")
+    @Description("Проверка, что пользователь может переключиться на вкладку 'Булки' и она становится активной")
     public void testUserCanSwitchToBunsTab() {
         MainPage mainPage = new MainPage(driver);
         mainPage.clickBunTab();
@@ -16,6 +20,8 @@ public class ConstructorTabTest extends BaseTest {
     }
 
     @Test
+    @DisplayName("Переключение на вкладку 'Соусы'")
+    @Description("Проверка, что пользователь может переключиться на вкладку 'Соусы' и она становится активной")
     public void testUserCanSwitchToSaucesTab() {
         MainPage mainPage = new MainPage(driver);
         mainPage.clickSauceTab();
@@ -23,6 +29,8 @@ public class ConstructorTabTest extends BaseTest {
     }
 
     @Test
+    @DisplayName("Переключение на вкладку 'Начинки'")
+    @Description("Проверка, что пользователь может переключиться на вкладку 'Начинки' и она становится активной")
     public void testUserCanSwitchToFillingsTab() {
         MainPage mainPage = new MainPage(driver);
         mainPage.clickFillingTab();
