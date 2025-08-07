36e89505d364a4fef0d31bb41a9089a7aee0f12d
rediyss <reddreynoklik@yandex.ru>
2025-08-07 18:35:45 +1100

    Добавлена аннотация step

--- src/test/java/praktikum/page/ForgotPasswordPage.java
+++ src/test/java/praktikum/page/ForgotPasswordPage.java
@@ -1,6 +1,6 @@
-
 package praktikum.page;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -12,6 +12,7 @@ public class ForgotPasswordPage {
         this.driver = driver;
     }
 
+    @Step("Клик по ссылке 'Войти' на странице восстановления пароля")
     public void clickLoginLink() {
         driver.findElement(loginLink).click();
     }
